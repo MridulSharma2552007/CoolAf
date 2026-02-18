e26f4f25238234853cb38986d886079ecb52e960
MridulSharma2552007 <mridul25sharma52007@gmail.com>
2026-02-19 00:15:27 +0530

    added comment documentation

--- boot.asm
+++ boot.asm
@@ -1,10 +1,10 @@
 [BITS 16]    ; Becaus cpu starts at 16 bits we need to set this
-[ORG 0X7C00]    ;this is the location where first 512 bytes of memory  BIOS loads 
+[ORG 0X7C00]    ;this is the location where first 512 bytes of memory  BIOS loads  
 
 start:
     mov ah, 0x0E ; this is a half part of a registed called AX =[AH][AL] 8x8 bits  and we are putting binaries of 0X0E which is used for printing ah is used here becaus we use int 0x10 that is video services , which seacrhed ah and all the data is stored in last 8 bits which is [AL] 
 
-    mov si, message ; si=Source index registed which works as a pointer to the message , hence stroe memory address of message
+    mov si, message ; si=Source index registed which works as a pointer to the message , hence stroe memory address of message 
 
 print:
     lodsb ; lodsb a Cpu instruction or we can say a loop it will put the data from source index register to the al one by one AL=[SI] SI++  
