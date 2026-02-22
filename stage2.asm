9b29b1de3f09ad5e6dee14e77dda4251a4a646df
MridulSharma2552007 <mridul25sharma52007@gmail.com>
2026-02-22 21:40:18 +0530

    fixed gdt 32 bit loading

--- stage2.asm
+++ stage2.asm
@@ -2,46 +2,95 @@
 [ORG 0x8000]
 
 start:
-call enable_a20
-mov ah,0x0E
-mov si,message
+    cli
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7C00
+    sti
+
+    call enable_a20
+
+    mov ah, 0x0E
+    mov si, message
 
 print:
-lodsb
-cmp al,0
-je done 
-int 0x10
-jmp print
-
-done:
-     cli ;disable interupts
-     lgdt[gdt_descriptor]; load gdt Global descriptor table
-     mov eax, cr0 ; read control register
-     or eax,1 ; 
-     mov cr0,eax ; write control register
-     jmp 0x08:protected_mode ; jump to protected mode code segment
+    lodsb
+    cmp al, 0
+    je switch_to_pm
+    int 0x10
+    jmp print
 
 
+; =========================
+; SWITCH TO PROTECTED MODE
+; =========================
+
+switch_to_pm:
+    cli
+    lgdt [gdt_descriptor]
+
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
+
+    jmp 0x08:pm_entry   ; FAR jump
+
+
+; =========================
+; A20
+; =========================
 
 enable_a20:
-     in al ,0x92 ; in means reads from a input output port of //here it reads form address 0x92 which has 8 bits  
-     or al, 00000010b; doins or operation btw al and 1 or we can say btw 0x92 and 1
-     out 0x92,al ; set al or results to 0x92 gurrentess that a20 is enabled 
-     ret; just a weired thing that keeps cpu in track i mean that it tell the computer taht what is the next thing he should execute
+    in al, 0x92
+    or al, 00000010b
+    out 0x92, al
+    ret
+
+
+; =========================
+; GDT
+; =========================
 
 gdt_start:
-
-gdt_null:
-     dq 0x0000000000000000; required by intel 
-gdt_code:
-     dq 0x00CF9A000000FFFF
-gdt_data:
-     dq 0x00CF92000000FFFF; for read and wqrite
+gdt_null: dq 0
+gdt_code: dq 0x00CF9A000000FFFF
+gdt_data: dq 0x00CF92000000FFFF
 gdt_end:
- 
- gdt_descriptor:
-     dw gdt_end - gdt_start -1 ; required by intel gdt=24 bits  24-1=23 8x4=24
-     dd  gdt_start
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
 
 
-message  db "Stage2 Loaded!",0
+message db "Stage2 Loaded!", 0
+
+
+; =========================
+; 32 BIT SECTION
+; =========================
+
+[BITS 32]
+
+pm_entry:
+
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+
+    mov esp, 0x90000
+
+    ; CLEAR SCREEN FULLY (VERY OBVIOUS)
+    mov edi, 0xB8000
+    mov ecx, 80*25
+    mov ax, 0x1F41    ; 'A' bright white on blue
+
+clear_screen:
+    mov [edi], ax
+    add edi, 2
+    loop clear_screen
+
+hang:
+    jmp hang
